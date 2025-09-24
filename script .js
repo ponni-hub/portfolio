6d3945fb471c09fe928c72617c24c9b52cabad2e
ponni <rr6234263@gmail.com>
2025-09-24 12:45:47 +0530

    update background

--- script .js
+++ script .js
@@ -15,6 +15,7 @@
         //     document.querySelectorAll('.nav-links a').forEach(link => {
         //         link.addEventListener('click', () => {
         //             navLinks.classList.remove('active');
+        
         //         });
         //     });
             
